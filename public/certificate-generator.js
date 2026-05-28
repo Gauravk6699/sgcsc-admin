2daf6a300545c35cead90338b3e8bf83180fc6da
Gaurav <gauravflack@gmail.com>
2026-05-29 03:22:44 +0530

    Fix QR code rendering and cleanup code

--- public/certificate-generator.js
+++ public/certificate-generator.js
@@ -103,14 +103,6 @@ var CertificateGenerator = (() => {
       _ctx.fillText('QR', x + size/2, y + size/2 + 5);
       _ctx.restore();
     }
-      console.warn('QR failed:', e.message);
-      _ctx.save();
-      _ctx.strokeStyle = '#000'; _ctx.lineWidth = 2;
-      _ctx.strokeRect(x, y, size, size);
-      _ctx.fillStyle = '#000'; _ctx.font = '16px serif'; _ctx.textAlign = 'center';
-      _ctx.fillText('QR', x + size/2, y + size/2 + 5);
-      _ctx.restore();
-    }
   }
 
   function _drawField(field, text) {
